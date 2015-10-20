40ee123626ad5774ebb3234ec8aaab6ef1a52384
ndunhourowens <ndunhour@hotmail.com>
2015-10-19 22:29:49 -1000

    sets variables to use express and router.  set up server on port 3000

--- app.js
+++ app.js
@@ -7,33 +7,15 @@ app.set('views', './views');
 
 app.use(express.static('./views'));
 
+// pointer to routes/gallery.js
+app.use('/gallery', gallery);
+
+
+
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().address;
 });
 
-// GET/ ==> view list of gallery photos
-
-// GET/gallery/:id ==> see single gallery photo
-  // photo include a link to delete photo
-  // photo link to edit photo
-
-// GET/gallery/new ==> form
-  // fields:
-    // author: Text
-    // link: Text(imgage URL)
-    // description: TextArea
-
-// POST/gallery ==> new gallery photo
-
-// GET/gallery/:id/edit ==> edit form identified by :id
-  // fields:
-    //author: Text
-    // link: Text(image URL)
-    // description: TextArea
-
-// PUT/gallery/:id ==> updates single photo
-
-// DELETE/gallery/:id ==> delete single photo
 
 module.exports = router;
