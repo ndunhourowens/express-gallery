f151235d5dbad67f110dfbd89247b022a882caa8
ndunhourowens <ndunhour@hotmail.com>
2015-10-20 10:37:24 -1000

    creating files to implement a functioning html page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var gallery = require('./routes/gallery');
 var router = express.Router();
 
 app.set('view engine', 'jade');
@@ -10,9 +11,16 @@ app.use(express.static('./views'));
 // pointer to routes/gallery.js
 app.use('/gallery', gallery);
 
+app.get('/', function(req, res){
+  res.render('index', {
+    title: '_ARCHITEKT'
+  });
+});
+
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().address;
+  console.log('testing');
 });
 
 
--- routes/gallery.js
+++ routes/gallery.js
@@ -3,42 +3,40 @@ var express = require('express');
 var router = express.Router();
 
 // GET/ ==> view list of gallery photos
-router.route('/')
-  .get(function(req, res){
-    res.send('gallery home page');
-  })
-  // POST/gallery ==> new gallery photo
-  .post(function(req, res){
-    res.send('gallery post page');
-  });
+router.get('/', function(req, res){
+  res.send('gallery home page');
+});
+// POST/gallery ==> new gallery photo
+router.post('/', function(req, res){
+  res.send('gallery post page');
+});
 
 
 // GET/gallery/:id ==> see single gallery photo
   // photo include a link to delete photo
   // photo link to edit photo
-router.route('/:id')
-  .get(function(req, res){
-    res.send('see single photo');
-  })
+router.get('/:id', function(req, res){
+  res.send('see single photo');
+});
 
   // PUT/gallery/:id ==> updates single photo
-  .put(function(req, res){
-    res.send('PUT single photo');
-  })
+router.put('/:id', function(req, res){
+  res.send('PUT single photo');
+});
 
   // DELETE/gallery/:id ==> delete single photo
-  .delete(function(req, res){
-    res.send('DELETE single photo');
-  });
+router.delete('/:id', function(req, res){
+  res.send('DELETE single photo');
+});
 
 // GET/gallery/:id/edit ==> edit form identified by :id
   // fields:
     //author: Text
     // link: Text(image URL)
     // description: TextArea
-route.route('/:id/edit', function(req, res){
-  res.send('edit single photo');
-});
+// route.route('/:id/edit', function(req, res){
+//   res.send('edit single photo');
+// });
 
 
 // GET/gallery/new ==> form
@@ -46,7 +44,7 @@ route.route('/:id/edit', function(req, res){
     // author: Text
     // link: Text(imgage URL)
     // description: TextArea
-router.get('/new', function(req, res){
+router.get('/new/', function(req, res){
   res.send('new form for user to fill');
 });
 
